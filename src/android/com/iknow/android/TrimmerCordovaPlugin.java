ca5da5056d5bb3a0f1893068afb630ee885528a6
chenqijing <qijing@wo.cn>
2018-07-28 00:05:55 +0800

    调试

--- src/android/com/iknow/android/TrimmerCordovaPlugin.java
+++ src/android/com/iknow/android/TrimmerCordovaPlugin.java
@@ -1,6 +1,14 @@
 package com.iknow.android;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+
+import com.iknow.android.features.select.VideoSelectActivity;
+import com.iknow.android.features.trim.VideoTrimmerActivity;
+import com.iknow.android.models.VideoInfo;
+import com.iknow.android.utils.TrimVideoUtil;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
@@ -10,14 +18,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
+import iknow.android.utils.callback.SimpleCallback;
+import io.reactivex.functions.Consumer;
+
 public class TrimmerCordovaPlugin extends CordovaPlugin {
 
-    private static TrimmerCordovaPlugin instance;
-    private static Activity activity;
+    public static TrimmerCordovaPlugin instance;
+    private Activity activity;
+    private int REQUEST_GET_TIME = 1;
 
-    public TrimmerCordovaPlugin(){
-        instance = this;
-
+    public TrimmerCordovaPlugin() {
+        TrimmerCordovaPlugin.instance = this;
     }
 
     @Override
@@ -33,8 +46,19 @@ public class TrimmerCordovaPlugin extends CordovaPlugin {
 
         if (action.equals("openTrimmerPage")) {
             // todo
-            String path= jsObj.getString("path");
-            callbackContext.success(path);
+            String path = jsObj.getString("path");
+            final Intent intent = new Intent();
+            intent.setClass(this.activity, VideoSelectActivity.class);
+            intent.putExtra("path", path);
+            this.activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    TrimmerCordovaPlugin.this.activity.startActivity(intent);
+                }
+            });
+
+
+            //callbackContext.success(path);
             return true;
         }
 
