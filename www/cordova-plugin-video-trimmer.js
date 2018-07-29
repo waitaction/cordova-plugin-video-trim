6083284d50f065c1c81de4127ba8e38c94ec4265
chenqijing <qijing@wo.cn>
2018-07-29 17:44:22 +0800

    调试

--- www/cordova-plugin-video-trimmer.js
+++ www/cordova-plugin-video-trimmer.js
@@ -8,26 +8,40 @@ Trimmer.prototype.openTrimmerPage = function (opt, success, error) {
 }
 
 /**打开本地视频列表页 */
-Trimmer.prototype.openSelectVideoPage = function (opt,success, error) {
+Trimmer.prototype.openSelectVideoPage = function (opt, success, error) {
     exec(success, error, "CordovaTrimmer", "openSelectVideoPage", [opt]);
 }
 
 /**播放 */
 Trimmer.prototype.play = function (opt, success, error) {
-    exec(success, error, "CordovaTrimmer", "play", [opt]);
+    // Play a video with callbacks
+    var options = {
+        successCallback: function () {
+            console.log("Video was closed without error.");
+            success && success();
+        },
+        errorCallback: function (errMsg) {
+            console.log("Error! " + errMsg);
+            error && error(errMsg);
+        },
+        orientation: 'portrait',//portrait landscape
+        shouldAutoClose: true,  // true(default)/false
+        controls: true // true(default)/false. Used to hide controls on fullscreen
+    };
+    window.plugins.streamingMedia.playVideo(opt.path, options);
 }
 
 /**打开摄像头录制视频 */
 Trimmer.prototype.openRecordVideoPage = function (success, error) {
     navigator.device.capture.captureVideo(function (mediaFiles) {
-          exec(success, error, "CordovaTrimmer", "openTrimmerPage", [{path:mediaFiles[0].fullPath}]);
+        exec(success, error, "CordovaTrimmer", "openTrimmerPage", [{ path: mediaFiles[0].fullPath }]);
     }, function (err) {
         error(err);
     }, { limit: 1, duration: 20 });
 }
 
 Trimmer.prototype.init = function (success, error) {
-     exec(success, error, "CordovaTrimmer", "init", []);
+    exec(success, error, "CordovaTrimmer", "init", []);
 }
 
 module.exports = new Trimmer();
