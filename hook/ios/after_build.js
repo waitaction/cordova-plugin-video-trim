7e8ae33d9c56bfa5023f80be725528c2bdc9105a
chenqijing <qijing@wo.cn>
2020-04-19 13:09:55 +0800

    。

--- hook/ios/after_build.js
+++ hook/ios/after_build.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 var cameraPath = "platforms/ios/" + getName() + "/Plugins/cordova-plugin-camera/CDVCamera.m";
-var capturePath = "platforms/ios/" + getName() + "/Plugins/cordova-plugin-camera/CDVCapture.m";
+var capturePath = "platforms/ios/" + getName() + "/Plugins/cordova-plugin-media-capture/CDVCapture.m";
 
 function iOSTrim() {
     var data = fs.readFileSync(cameraPath, 'utf8');
@@ -15,8 +15,48 @@ function iOSTrim() {
 }
 
 function fixedCDVCapture() {
+    var sCode =
+        `- (void)imagePickerController:(UIImagePickerController*)picker didFinishPickingMediaWithInfo:(NSDictionary*)info\n` +
+        `{\n` +
+        `    CDVImagePicker* cameraPicker = (CDVImagePicker*)picker;\n` +
+        `    NSString* callbackId = cameraPicker.callbackId;\n` +
+        `\n` +
+        `    [[picker presentingViewController] dismissViewControllerAnimated:YES completion:nil];\n` +
+        `\n` +
+        `    CDVPluginResult* result = nil;\n` +
+        `\n` +
+        `    UIImage* image = nil;\n` +
+        `    NSString* mediaType = [info objectForKey:UIImagePickerControllerMediaType];\n` +
+        `    if (!mediaType || [mediaType isEqualToString:(NSString*)kUTTypeImage]) {\n` +
+        `        // mediaType is nil then only option is UIImagePickerControllerOriginalImage\n` +
+        `        if ([UIImagePickerController respondsToSelector:@selector(allowsEditing)] &&\n` +
+        `            (cameraPicker.allowsEditing && [info objectForKey:UIImagePickerControllerEditedImage])) {\n` +
+        `            image = [info objectForKey:UIImagePickerControllerEditedImage];\n` +
+        `        } else {\n` +
+        `            image = [info objectForKey:UIImagePickerControllerOriginalImage];\n` +
+        `        }\n` +
+        `    }\n` +
+        `    if (image != nil) {\n` +
+        `        // mediaType was image\n` +
+        `        result = [self processImage:image type:cameraPicker.mimeType forCallbackId:callbackId];\n` +
+        `    } else if ([mediaType isEqualToString:(NSString*)kUTTypeMovie]) {\n` +
+        `        // process video\n` +
+        `        NSString* moviePath = [(NSURL *)[info objectForKey:UIImagePickerControllerMediaURL] path];\n` +
+        `        if (moviePath) {\n` +
+        `            result = [self processVideo:moviePath forCallbackId:callbackId];\n` +
+        `        }\n` +
+        `    }\n` +
+        `    if (!result) {\n` +
+        `        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageToErrorObject:CAPTURE_INTERNAL_ERR];\n` +
+        `    }\n` +
+        `    [self.commandDelegate sendPluginResult:result callbackId:callbackId];\n` +
+        `    pickerController = nil;\n` +
+        `}`;
+
+
     var code = `- (void)imagePickerController:(UIImagePickerController*)picker didFinishPickingMediaWithInfo:(NSDictionary*)info
 {
+    //这段方法由cordova-plugin-video-trim注入
     __weak CDVImagePicker* cameraPicker = (CDVImagePicker*)picker;
     __weak CDVCapture* weakSelf = self;
     NSString* callbackId = cameraPicker.callbackId;
@@ -51,15 +91,13 @@ function fixedCDVCapture() {
     };
     [[picker presentingViewController] dismissViewControllerAnimated:YES completion:invoke];
 }
-- (void)imagePickerControllerDidCancel:
 `;
     var data = fs.readFileSync(capturePath, 'utf8');
     if (data.indexOf(`__weak CDVImagePicker* cameraPicker = (CDVImagePicker*)picker;`) > 0) {
         return;
     }
-    content = data.replace(/\- \(void\)imagePickerController:[\s\S]*\- \(void\)imagePickerControllerDidCancel:/ig,
-        code);
-    fs.writeFileSync(cameraPath, content, 'utf8');
+    content = data.replace(sCode, code);
+    fs.writeFileSync(capturePath, content, 'utf8');
 }
 
 function getName() {
